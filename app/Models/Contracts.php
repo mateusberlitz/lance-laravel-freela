e776561c7ea92be4a1b072279bd65b97f6c6309f
Alvin Scheibe <alvin.scheibe@gmail.com>
2022-03-20 20:28:36 -0300

    Add relation model between contracts and customers

--- app/Models/Contracts.php
+++ app/Models/Contracts.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contracts extends Model {
 
@@ -10,5 +11,9 @@ class Contracts extends Model {
     protected $fillable = [
         'number_contract', 'customer_id'
     ];
+
+    public function customer(): BelongsTo {
+        return $this->belongsTo(Customers::class);
+    }
 
 }
